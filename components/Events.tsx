7d23ca5e0faf5cfeb6c719852348427b091a50b4
zaynahanjum <zaynahanjum789@gmail.com>
2026-09-22 18:37:04 +0530

    cleared lint warning

--- components/Events.tsx
+++ components/Events.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { events } from "@/assets/data/events";
-import Button from "./Button";
+// import Button from "./Button";
 
 gsap.registerPlugin(ScrollTrigger);
 
