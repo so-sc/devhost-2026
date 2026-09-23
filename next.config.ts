751ea0e70da4b5ef49284c9f042624c2da87910a
zaynahanjum <zaynahanjum789@gmail.com>
2026-09-23 19:08:00 +0530

    corrected links

--- next.config.ts
+++ next.config.ts
@@ -11,27 +11,27 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/ctf/register",
-        destination: "https://unstop.com/m/opportunity/1738040/step3",
+        destination: "https://unstop.com/p/siege-of-troy-ctf-devhost-2026-sahyadri-open-source-community-1738040",
         permanent: false,
       },
       {
         source: "/cp/register",
-        destination: "https://unstop.com/m/opportunity/1738042/step3",
+        destination: "https://unstop.com/p/the-sphinx-trial-cp-devhost-2026-sahyadri-open-source-community-1738042",
         permanent: false,
       },
       {
         source: "/tech-pitch/register",
-        destination: "https://unstop.com/m/opportunity/1738041/step3",
+        destination: "https://unstop.com/p/voice-of-the-oracle-tech-pitch-devhost-2026-sahyadri-open-source-community-1738041",
         permanent: false,
       },
       {
         source: "/content-creation/register",
-        destination: "https://unstop.com/m/opportunity/1760229/step3",
+        destination: "https://unstop.com/p/mythic-canvas-content-creation-competition-devhost-2026-sahyadri-open-source-community-1760229?section=contact",
         permanent: false,
       },
       {
         source: "/bgmi/register",
-        destination: "https://unstop.com/m/opportunity/1737815/step3",
+        destination: "https://unstop.com/p/arena-of-ares-bgmi-devhost-2026-sahyadri-open-source-community-1737815",
         permanent: false,
       },
     ];
