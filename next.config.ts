cae3bbcc9a2f074d8b6f5cbe15047b38279c8892
zaynahanjum <zaynahanjum789@gmail.com>
2026-09-24 10:01:16 +0530

    solved button overlap issue

--- next.config.ts
+++ next.config.ts
@@ -11,27 +11,32 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/ctf/register",
-        destination: "https://unstop.com/p/siege-of-troy-ctf-devhost-2026-sahyadri-open-source-community-1738040",
+        destination:
+          "https://unstop.com/p/siege-of-troy-ctf-devhost-2026-sahyadri-open-source-community-1738040",
         permanent: false,
       },
       {
         source: "/cp/register",
-        destination: "https://unstop.com/p/the-sphinx-trial-cp-devhost-2026-sahyadri-open-source-community-1738042",
+        destination:
+          "https://unstop.com/p/the-sphinx-trial-cp-devhost-2026-sahyadri-open-source-community-1738042",
         permanent: false,
       },
       {
         source: "/tech-pitch/register",
-        destination: "https://unstop.com/p/voice-of-the-oracle-tech-pitch-devhost-2026-sahyadri-open-source-community-1738041",
+        destination:
+          "https://unstop.com/p/voice-of-the-oracle-tech-pitch-devhost-2026-sahyadri-open-source-community-1738041",
         permanent: false,
       },
       {
         source: "/content-creation/register",
-        destination: "https://unstop.com/p/mythic-canvas-content-creation-competition-devhost-2026-sahyadri-open-source-community-1760229?section=contact",
+        destination:
+          "https://unstop.com/p/mythic-canvas-content-creation-competition-devhost-2026-sahyadri-open-source-community-1760229?section=contact",
         permanent: false,
       },
       {
         source: "/bgmi/register",
-        destination: "https://unstop.com/p/arena-of-ares-bgmi-devhost-2026-sahyadri-open-source-community-1737815",
+        destination:
+          "https://unstop.com/p/arena-of-ares-bgmi-devhost-2026-sahyadri-open-source-community-1737815",
         permanent: false,
       },
     ];
